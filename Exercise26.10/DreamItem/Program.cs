8dced07db875c9ec44c30d73ed83d3e49bd4ba38
Radeff <rostislavradev@gmail.com>
2016-10-26 17:13:35 +0300

    From Laptop - Exercise 26.10

---
+++ Exercise26.10/DreamItem/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamItem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string month = input.Split('\\')[0];
+            var mph = decimal.Parse(input.Split('\\')[1]);
+            var hpd = uint.Parse(input.Split('\\')[2]);
+            var price = decimal.Parse(input.Split('\\')[3]);
+
+            int workdays = 0;
+            switch (month)
+            {
+                case "Jan":
+                    workdays = 31;
+                break;
+                case "Feb":
+                    workdays = 28;
+                    break;
+                case "March":
+                    workdays = 31;
+                    break;
+                case "Apr":
+                    workdays = 30;
+                    break;
+                case "May":
+                    workdays = 31;
+                    break;
+                case "June":
+                    workdays = 30;
+                    break;
+                case "July":
+                    workdays = 31;
+                    break;
+                case "Aug":
+                    workdays = 31;
+                    break;
+                case "Sept":
+                    workdays = 30;
+                    break;
+                case "Oct":
+                    workdays = 31;
+                    break;
+                case "Nov":
+                    workdays = 30;
+                    break;
+                case "Dec":
+                    workdays = 31;
+                    break;
+            }
+            workdays -= 10;
+            var wage = workdays * (mph * hpd);
+            if (wage > 700)
+            {
+                wage *= 1.1M;
+            }
+            if (wage < price)
+            {
+                Console.WriteLine("Not enough money. {0:F2} leva needed.", price - wage);
+            }
+            else
+            {
+                Console.WriteLine("Money left = {0:F2} leva.", wage - price);
+            }
+        }
+    }
+}
---
+++ Exercise26.10/GrandTheftExamo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandTheftExamo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long thievesSlap = 0;
+            long thievesEsc = 0;
+            long totalBeer = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var thieves = long.Parse(Console.ReadLine());
+                var beer = long.Parse(Console.ReadLine());
+                if (thieves > 5)
+                {
+                    thievesSlap += 5;
+                    thievesEsc += thieves - 5;
+                }
+                else
+                {
+                    thievesSlap += thieves;
+                }
+                totalBeer += beer;       
+            }
+            Console.WriteLine(thievesSlap + " thieves slapped.");
+            Console.WriteLine(thievesEsc + " thieves escaped.");
+            Console.WriteLine(totalBeer / 6 + " packs, " + totalBeer % 6 + " bottles."); 
+        }
+    }
+}

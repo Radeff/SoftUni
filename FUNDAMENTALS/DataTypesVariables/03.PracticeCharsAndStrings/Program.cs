b7ae660922fb3ef652141ea8407ec2db099a5dbb
Rostislav <radeff@radeff.eu>
2017-01-20 16:18:34 +0200

    Solved tasks 02 & 03

---
+++ FUNDAMENTALS/DataTypesVariables/03.PracticeCharsAndStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.PracticeCharsAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Software University";
+            char char1 = 'B';
+            char char2 = 'y';
+            char char3 = 'e';
+            string str2 = "I love programming";
+            Console.WriteLine(str1);
+            Console.WriteLine(char1);
+            Console.WriteLine(char2);
+            Console.WriteLine(char3);
+            Console.WriteLine(str2);
+        }
+    }
+}

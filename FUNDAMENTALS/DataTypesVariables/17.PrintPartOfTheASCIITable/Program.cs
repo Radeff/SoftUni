9ea6f00d0e0434678b80e073dbc9139ede6ec045
Rostislav <radeff@radeff.eu>
2017-01-20 23:39:17 +0200

    Task 17 Solved

---
+++ FUNDAMENTALS/DataTypesVariables/17.PrintPartOfTheASCIITable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.PrintPartOfTheASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startChar = int.Parse(Console.ReadLine());
+            int endChar = int.Parse(Console.ReadLine());
+            for (int i = startChar; i <= endChar; i++)
+            {
+                Console.Write(Convert.ToChar(i)+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

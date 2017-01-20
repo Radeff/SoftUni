f8bf0246e6bfe5e20c24fc06b90e8656ee6fc871
Rostislav <radeff@radeff.eu>
2017-01-20 20:17:34 +0200

    Task 11 Solved

---
+++ FUNDAMENTALS/DataTypesVariables/11.ConvertSpeedUnits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.ConvertSpeedUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int seconds = int.Parse(Console.ReadLine());
+
+            double totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
+            double metersPerSecond = meters / totalSeconds;
+            Console.WriteLine(Math.Round(metersPerSecond, 6));
+            double kmPerHour = (meters / 1000.00) / (totalSeconds / 3600.00);
+            Console.WriteLine(Math.Round(kmPerHour, 6));
+            double milesPerHour = (meters / 1609.00) / (totalSeconds / 3600.00);
+            Console.WriteLine(Math.Round(milesPerHour, 6));
+        }
+    }
+}

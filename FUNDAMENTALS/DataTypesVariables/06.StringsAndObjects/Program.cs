e76b8b732553096ff2774d2721ab746d4474d2ad
Rostislav <radeff@radeff.eu>
2017-01-20 17:01:35 +0200

    Task 06 Solved

---
+++ FUNDAMENTALS/DataTypesVariables/06.StringsAndObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.StringsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            object obj = str1 + ' ' + str2;
+            string str3 = (string)obj;
+            Console.WriteLine(str3);
+        }
+    }
+}

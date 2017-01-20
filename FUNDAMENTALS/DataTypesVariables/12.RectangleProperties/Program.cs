8ae6ef446ea8066fd53b91d132d6268b2ef8d7dd
Rostislav <radeff@radeff.eu>
2017-01-20 20:39:41 +0200

    Task 12 Solved

--- FUNDAMENTALS/DataTypesVariables/12.RectangleProperties/Program.cs
+++ FUNDAMENTALS/DataTypesVariables/12.RectangleProperties/Program.cs
@@ -10,6 +10,14 @@ namespace _12.RectangleProperties
     {
         static void Main(string[] args)
         {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double perimeter = (a + b) * 2;
+            double area = a * b;
+            double diagonal = Math.Sqrt(a * a + b * b);
+            Console.WriteLine(perimeter);
+            Console.WriteLine(area);
+            Console.WriteLine(diagonal);
         }
     }
 }

0ab08b6355be6cbb849dd937be462707399a2c91
Rostislav <radeff@radeff.eu>
2017-01-20 17:47:10 +0200

    Task 09 Solved

---
+++ FUNDAMENTALS/DataTypesVariables/09.ReverseCharacters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.ReverseCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letter1 = Console.ReadLine();
+            string letter2 = Console.ReadLine();
+            string letter3 = Console.ReadLine();
+            Console.WriteLine(letter3 + letter2 + letter1);
+        }
+    }
+}

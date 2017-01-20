d81fe95297b61d8651748ad2d17f62fdc74b34df
Rostislav <radeff@radeff.eu>
2017-01-20 16:39:35 +0200

    Task 05 Solved

---
+++ FUNDAMENTALS/DataTypesVariables/05.BooleanVariable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.BooleanVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            var check = Convert.ToBoolean(str);
+            if (check)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}

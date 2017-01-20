724a4c32e4d8cef737169899b3621b5b26f2f778
Rostislav <radeff@radeff.eu>
2017-01-20 17:06:41 +0200

    Task 07 Solved

---
+++ FUNDAMENTALS/DataTypesVariables/07.ExchangeVariableValues/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.ExchangeVariableValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("Before:");
+            Console.WriteLine("a = "+a);
+            Console.WriteLine("b = "+b);
+            Console.WriteLine("After:");
+            Console.WriteLine("a = "+b);
+            Console.WriteLine("b = "+a);
+        }
+    }
+}

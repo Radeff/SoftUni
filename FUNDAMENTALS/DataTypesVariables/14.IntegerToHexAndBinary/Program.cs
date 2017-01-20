7eb6d653be5444b2169c5704f7504c9296c5eef2
Rostislav <radeff@radeff.eu>
2017-01-20 22:27:39 +0200

    Tasks 14 & 15 Solved

---
+++ FUNDAMENTALS/DataTypesVariables/14.IntegerToHexAndBinary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.IntegerToHexAndBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            string text = Convert.ToString(input, 16);
+            text = text.ToUpper();
+            Console.WriteLine(text);
+            string binary = Convert.ToString(input, 2);
+            Console.WriteLine(binary);
+        }
+    }
+}
---
+++ FUNDAMENTALS/DataTypesVariables/15.FastPrimeCheckerRefactor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.FastPrimeCheckerRefactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                bool checkPrime = true;
+                for (int divider = 2; divider <= Math.Sqrt(i); divider++)
+                {
+                    if (i % divider == 0)
+                    {
+                        checkPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {checkPrime}");
+            }
+
+        }
+    }
+}

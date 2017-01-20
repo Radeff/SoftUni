1b7eaaad49e26b1f6455f2ef771e6e36d292b019
Rostislav <radeff@radeff.eu>
2017-01-20 20:58:20 +0200

    Task 13 Solved

---
+++ FUNDAMENTALS/DataTypesVariables/VowelOrDigit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelOrDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char symbol = Convert.ToChar(input);
+            int ascii = Convert.ToInt16(symbol);
+            if (ascii >= 48 && ascii <= 57)
+            {
+                Console.WriteLine("digit");
+            }
+            else if (ascii == 97 || ascii == 101 || ascii == 105 || ascii == 111 || ascii == 117)
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}

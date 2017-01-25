2604faa22d71c1de3ed7033c3adc0b13a9136f5d
Rostislav <radeff@radeff.eu>
2017-01-26 01:29:19 +0200

    Task 08 Solved

---
+++ FUNDAMENTALS/MethodsАndDebugging/08.CenterPoint/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.CenterPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x1 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine());
+            var x2 = double.Parse(Console.ReadLine());
+            var y2 = double.Parse(Console.ReadLine());
+
+            var result = FindPointClosestToCenter(x1, y1, x2, y2);
+
+            Console.WriteLine(result);
+        }
+
+        public static string FindPointClosestToCenter(double x1, double y1, double x2, double y2)
+        {
+            if (FindDistanceToCenter(x1, y1) < FindDistanceToCenter(x2, y2))
+            {
+                return $"({x1}, {y1})";
+            }
+            else
+            {
+                return $"({x2}, {y2})";
+            }
+        }
+
+        public static double FindDistanceToCenter(double sideA, double sideB)
+        {
+            // Distance to center is actually the hypotenuse of a right-angle triangle
+            var hypotenuseDistance = Math.Sqrt((sideA * sideA) + (sideB * sideB));
+            return hypotenuseDistance;
+        }  
+    }
+}

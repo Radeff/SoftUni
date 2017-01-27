f095896a80519befed1967c46ef4a1f4b2799b01
Rostislav <radeff@radeff.eu>
2017-01-27 12:49:55 +0200

    Task 15 Solved

---
+++ FUNDAMENTALS/MethodsАndDebugging/15.Substring/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Substring_broken
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        int jump = int.Parse(Console.ReadLine());
+
+        const char search = 'p';
+        bool hasMatch = false;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == search)
+            {
+                hasMatch = true;
+                int endIndex = jump + 1;
+
+                if (endIndex + i >= text.Length)
+                {
+                    endIndex = text.Length - i;
+                }
+
+                string matchedString = text.Substring(i, endIndex);
+                Console.WriteLine(matchedString);
+                i += jump;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        if (hasMatch == false)
+        {
+            Console.WriteLine("no");
+        }
+    }
+}

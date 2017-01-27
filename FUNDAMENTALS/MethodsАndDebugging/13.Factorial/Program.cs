8594f58679306d9f5e82da22366edd09425e8fb2
Rostislav <radeff@radeff.eu>
2017-01-27 11:15:38 +0200

    Task 13 Solved

---
+++ FUNDAMENTALS/MethodsАndDebugging/13.Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace _13.Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            PrintFactorial(n);
+        }
+
+        public static void PrintFactorial(int n)
+        {
+            BigInteger fact = 1;
+            while (n > 0)
+            {
+                fact = fact * n;
+                n--;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}

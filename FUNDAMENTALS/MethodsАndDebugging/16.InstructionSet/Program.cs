fa0f9930a090ffe6e586ea9e2ee66f910e9cc054
Rostislav <radeff@radeff.eu>
2017-01-27 14:47:15 +0200

    Task 16 Solved

---
+++ FUNDAMENTALS/MethodsАndDebugging/16.InstructionSet/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class InstructionSet_broken
+{
+    static void Main()
+    {
+        string opCode = Console.ReadLine();
+        
+        long result = 0;
+
+        while (opCode != "END")
+        {
+            string[] codeArgs = opCode.Split(' ');
+            switch (codeArgs[0])
+            {
+                case "INC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        result = ++operandOne;
+                        break;
+                    }
+                case "DEC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        result = --operandOne;
+                        break;
+                    }
+                case "ADD":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result = operandOne + operandTwo;
+                        break;
+                    }
+                case "MLA":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result = operandOne * operandTwo;
+                        break;
+                    }
+            }
+            Console.WriteLine(result);
+            opCode = Console.ReadLine();
+        }
+    }
+}

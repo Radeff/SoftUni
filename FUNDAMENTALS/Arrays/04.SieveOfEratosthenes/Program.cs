b5a117198aca08c08fb467c5ffc99f869887981f
Rostislav <radeff@radeff.eu>
2017-01-31 01:17:33 +0200

    Task 04 Solved

---
+++ FUNDAMENTALS/Arrays/04.SieveOfEratosthenes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04.SieveOfEratosthenes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var primes = new bool[n + 1];
+            primes[0] = primes[1] = false;
+            for (int i = 2; i < primes.Length; i++)
+            {
+                primes[i] = true;
+            }
+
+            FindAndPrintPrimesToN(primes, n);
+        }
+
+        public static void FindAndPrintPrimesToN(bool[] primes, int n)
+        {
+            for (int i = 0; i < primes.Length; i++)
+            {
+                if (primes[i] == true)
+                {
+                    Console.Write(i + " ");
+                    for (int q = 2 * i; q < primes.Length; q += i)
+                    {
+                        primes[q] = false;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}

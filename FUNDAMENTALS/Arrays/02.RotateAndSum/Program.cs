42c2e84e5bc1f8d007f35d017bda49332fac46af
Rostislav <radeff@radeff.eu>
2017-01-30 04:31:49 +0200

    Task 02 Solved

---
+++ FUNDAMENTALS/Arrays/02.RotateAndSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.RotateAndSum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int k = int.Parse(Console.ReadLine());
+
+            RotateAndSumArray(numbers, k);
+        }
+
+        public static void RotateAndSumArray(int[] numbers, int k)
+        {
+            var sum = new int[numbers.Length];
+            for (int i = 1; i <= k; i++)
+            {
+                var lastNumber = numbers[numbers.Length - 1];
+
+                for (int j = numbers.Length - 1; j > 0; j--)
+                {
+                    numbers[j] = numbers[j - 1];
+
+                    sum[j] += numbers[j];
+                }
+
+                numbers[0] = lastNumber;
+                sum[0] += numbers[0];
+            }
+
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}

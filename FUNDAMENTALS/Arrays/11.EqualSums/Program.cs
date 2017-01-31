5d6bf6cbc1cef328ce95c99949c39319d680c000
Rostislav <radeff@radeff.eu>
2017-01-31 14:15:18 +0200

    Task 11 Solved

---
+++ FUNDAMENTALS/Arrays/11.EqualSums/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _11.EqualSums
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var intArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            FindAndPrintEqualSumsElement(intArray);
+        }
+
+        public static void FindAndPrintEqualSumsElement(int[] intArray)
+        {
+            var found = false;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                var leftSum = 0;
+                var rightSum = 0;
+
+                for (int q = 0; q < i; q++)
+                {
+                    leftSum += intArray[q];
+                }
+
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    rightSum += intArray[j];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    found = true;
+                    Console.WriteLine(i);
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}

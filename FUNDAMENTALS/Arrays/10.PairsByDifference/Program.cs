a258161738a10c281657554cb03a9e17c653099e
Rostislav <radeff@radeff.eu>
2017-01-31 14:01:50 +0200

    Task 10 Solved

---
+++ FUNDAMENTALS/Arrays/10.PairsByDifference/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.PairsByDifference
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var intArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var difference = int.Parse(Console.ReadLine());
+
+            FindCountPairsByDifference(intArray, difference);
+        }
+
+        public static void FindCountPairsByDifference(int[] intArray, int diff)
+        {
+            var counter = 0;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    if (Math.Abs(intArray[i] - intArray[j]) == diff)
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}

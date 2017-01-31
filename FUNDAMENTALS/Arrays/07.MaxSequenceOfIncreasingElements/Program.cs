a75cb70ba846cb99d87596906a57569081efd699
Rostislav <radeff@radeff.eu>
2017-01-31 12:50:39 +0200

    Task 07 Solved

---
+++ FUNDAMENTALS/Arrays/07.MaxSequenceOfIncreasingElements/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _07.MaxSequenceOfIncreasingElements
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var intArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            FindLongestSequelOfIncreasing(intArray);
+        }
+
+        public static void FindLongestSequelOfIncreasing(int[] intArray)
+        {
+            var previous = intArray[0];
+            var bestCounter = 0;
+            var bestPos = intArray.Length;
+            var counter = 0;
+            var startPos = intArray.Length;
+
+            for (int i = 1; i < intArray.Length; i++)
+            {
+                if (previous < intArray[i])
+                {
+                    counter++;
+                    startPos = i - counter;
+
+                    if (counter > bestCounter)
+                    {
+                        bestCounter = counter;
+                        bestPos = startPos;
+                    }
+
+                    previous = intArray[i];
+                }
+
+                else
+                {
+                    counter = 0;
+                    previous = intArray[i];
+                }
+
+            }
+
+            if (bestCounter > 0)
+            {
+                for (int i = bestPos; i <= bestPos + bestCounter; i++)
+                {
+                    Console.Write($"{intArray[i]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}

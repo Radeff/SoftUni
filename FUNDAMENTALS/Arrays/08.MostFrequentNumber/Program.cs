61ab8df62054279fb260a06683f2c67f549b9bbb
Rostislav <radeff@radeff.eu>
2017-01-31 13:21:14 +0200

    Task 08 Solved

---
+++ FUNDAMENTALS/Arrays/08.MostFrequentNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _08.MostFrequentNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var intArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            FindMostFrequentNumber(intArray);
+        }
+
+        public static void FindMostFrequentNumber(int[] intArray)
+        {
+            var bestCounter = 0;
+            var frequent = 0;
+            var bestFrequent = 0;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                var currentNum = intArray[i];
+                var counter = 0;
+
+                for (int j = 0; j < intArray.Length; j++)
+                {
+                    if (currentNum == intArray[j])
+                    {
+                        counter++;
+                        frequent = currentNum;
+
+                        if (counter > bestCounter)
+                        {
+                            bestCounter = counter;
+                            bestFrequent = frequent;
+                        }
+
+                    }
+                }
+            }
+            Console.WriteLine(bestFrequent);
+        }
+    }
+}
+

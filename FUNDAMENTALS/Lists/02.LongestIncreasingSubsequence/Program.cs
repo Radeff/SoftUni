def4e7253bd9d54e037be2e2201ca013c8960dc3
Rostislav <radeff@radeff.eu>
2017-02-03 19:37:23 +0200

    Task 02 Progress 91/100

--- FUNDAMENTALS/Lists/02.LongestIncreasingSubsequence/Program.cs
+++ FUNDAMENTALS/Lists/02.LongestIncreasingSubsequence/Program.cs
@@ -15,57 +15,59 @@ namespace _02.LongestIncreasingSubsequence
 
         public static void FindLongestIncreasingSubsequence(List<int> numbers)
         {
-            var longest = new List<int>();
-
             var currentLongest = new List<int>();
             currentLongest.Add(numbers[0]);
 
-            for (int j = 0; j < numbers.Count; j++)
+            var longest = new List<int>();
+            longest.Add(currentLongest[0]);
+
+            for (int i = 1; i < numbers.Count; i++)
             {
-                if (numbers[j] >= currentLongest.First())
+                if (numbers[i] > currentLongest.First())
                 {
-                    if (!currentLongest.Contains(numbers[j]))
+                    if (!currentLongest.Contains(numbers[i]))
                     {
-                        while (numbers[j] <= currentLongest.Last())
+                        while (numbers[i] <= currentLongest.Last())
                         {
                             currentLongest.Remove(currentLongest.Last());
                         }
 
-                        currentLongest.Add(numbers[j]);
+                        currentLongest.Add(numbers[i]);
+                    }
+
+                    if (currentLongest.Count > longest.Count)
+                    {
+                        longest.Clear();
+                        longest.AddRange(currentLongest);
                     }
                 }
+
                 else
                 {
                     var currentTemp = new List<int>();
-                    currentTemp.Add(numbers[j]);
+                    currentTemp.Add(numbers[i]);
 
-                    for (int q = j; q < numbers.Count; q++)
+                    for (int j = i; j < numbers.Count; j++)
                     {
-                        if (numbers[q] > currentTemp.First())
+                        if (numbers[j] > currentTemp.First())
                         {
-                            if (!currentTemp.Contains(numbers[q]))
+                            if (!currentTemp.Contains(numbers[j]))
                             {
-                                while (numbers[q] <= currentTemp.Last())
+                                while (numbers[j] <= currentTemp.Last())
                                 {
                                     currentTemp.Remove(currentTemp.Last());
                                 }
 
-                                currentTemp.Add(numbers[q]);
+                                currentTemp.Add(numbers[j]);
+                            }
+
+                            if (currentTemp.Count > longest.Count)
+                            {
+                                longest.Clear();
+                                longest.AddRange(currentTemp);
                             }
                         }
                     }
-
-                    if (currentTemp.Count > longest.Count)
-                    {
-                        longest.Clear();
-                        longest.AddRange(currentTemp);
-                    }
-                }
-
-                if (currentLongest.Count > longest.Count)
-                {
-                    longest.Clear();
-                    longest.AddRange(currentLongest);
                 }
             }
 

47c06ae968dba7bf65d0a6d3f8d5dff0b891bb9c
Rostislav <radeff@radeff.eu>
2017-02-03 05:50:02 +0200

    Task 02 Progress 83/100

---
+++ FUNDAMENTALS/Lists/02.LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.LongestIncreasingSubsequence
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var listOfNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            FindLongestIncreasingSubsequence(listOfNumbers);
+        }
+
+        public static void FindLongestIncreasingSubsequence(List<int> numbers)
+        {
+            var longest = new List<int>();
+
+            var currentLongest = new List<int>();
+            currentLongest.Add(numbers[0]);
+
+            for (int j = 0; j < numbers.Count; j++)
+            {
+                if (numbers[j] >= currentLongest.First())
+                {
+                    if (!currentLongest.Contains(numbers[j]))
+                    {
+                        while (numbers[j] <= currentLongest.Last())
+                        {
+                            currentLongest.Remove(currentLongest.Last());
+                        }
+
+                        currentLongest.Add(numbers[j]);
+                    }
+                }
+                else
+                {
+                    var currentTemp = new List<int>();
+                    currentTemp.Add(numbers[j]);
+
+                    for (int q = j; q < numbers.Count; q++)
+                    {
+                        if (numbers[q] > currentTemp.First())
+                        {
+                            if (!currentTemp.Contains(numbers[q]))
+                            {
+                                while (numbers[q] <= currentTemp.Last())
+                                {
+                                    currentTemp.Remove(currentTemp.Last());
+                                }
+
+                                currentTemp.Add(numbers[q]);
+                            }
+                        }
+                    }
+
+                    if (currentTemp.Count > longest.Count)
+                    {
+                        longest.Clear();
+                        longest.AddRange(currentTemp);
+                    }
+                }
+
+                if (currentLongest.Count > longest.Count)
+                {
+                    longest.Clear();
+                    longest.AddRange(currentLongest);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", longest));
+        }
+    }
+}

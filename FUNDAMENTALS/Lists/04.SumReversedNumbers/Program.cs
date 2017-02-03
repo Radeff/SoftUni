2e323f2941cd530a3810696e6d2af95106d61d63
Rostislav <radeff@radeff.eu>
2017-02-03 04:22:33 +0200

    Task 04 Solved

---
+++ FUNDAMENTALS/Lists/04.SumReversedNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SumReversedNumbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            PrintReversedSum(input);
+        }
+
+        public static void PrintReversedSum(string[] input)
+        { 
+            var numbers = new List<int>();
+            var sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                numbers.Add(int.Parse(new string(input[i].ToCharArray().Reverse().ToArray())));
+                sum += numbers[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}

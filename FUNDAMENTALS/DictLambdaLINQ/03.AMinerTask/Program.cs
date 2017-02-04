1d377e6876cd762dd330c45e048a07eba9e05796
Rostislav <radeff@radeff.eu>
2017-02-04 17:29:24 +0200

    Task 03 Solved

---
+++ FUNDAMENTALS/DictLambdaLINQ/03.AMinerTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.AMinerTask
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = string.Empty;
+            var ores = new Dictionary<string, int>();
+
+            while (input != "stop")
+            {
+                input = Console.ReadLine();
+                AddAndPrintOre(ores, input);
+            }            
+        }
+
+        public static void AddAndPrintOre(Dictionary<string, int> ores, string ore)
+        {            
+            if (ore == "stop")
+            {
+                foreach (var item in ores)
+                {
+                    Console.WriteLine($"{item.Key} -> {item.Value}");
+                }
+
+                return;
+            }
+
+            else if (!ores.ContainsKey(ore))
+            {
+                ores.Add(ore, int.Parse(Console.ReadLine()));
+            }       
+                 
+            else
+            {
+                ores[ore] += int.Parse(Console.ReadLine());
+            }            
+        }
+    }
+}

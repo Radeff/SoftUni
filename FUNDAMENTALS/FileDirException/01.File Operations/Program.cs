f19d7593552ce26bc460e06ebbd74a4de80bdbe6
Rostislav <radeff@radeff.eu>
2017-02-14 14:27:33 +0200

    Task 01 Solved

---
+++ FUNDAMENTALS/FileDirException/01.File Operations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace _01.File_Operations
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var text = File.ReadAllLines("text.txt");
+            var oddLines = new List<string>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    oddLines.Add(text[i]);
+                }
+            }
+
+            File.WriteAllLines("output.txt", oddLines);
+
+        }
+    }
+}

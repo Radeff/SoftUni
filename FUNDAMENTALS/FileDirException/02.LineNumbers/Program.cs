d959a4796a6cd90aeb21b57f331d12f162c53344
Rostislav <radeff@radeff.eu>
2017-02-14 15:52:53 +0200

    Task 02 Solved

---
+++ FUNDAMENTALS/FileDirException/02.LineNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.LineNumbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var lines = File.ReadAllLines("text.txt");
+            var numberedLines = new string[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {                
+                numberedLines[i] = string.Concat($"{i+1}. ", lines[i]);
+            }
+
+            File.WriteAllLines("output.txt", numberedLines);
+        }
+    }
+}

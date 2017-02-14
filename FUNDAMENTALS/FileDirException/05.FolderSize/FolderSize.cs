d66dd01cab5b140eee9ae2cce435915218c1d6bd
Rostislav <radeff@radeff.eu>
2017-02-14 18:25:50 +0200

    Task 05 Solved

---
+++ FUNDAMENTALS/FileDirException/05.FolderSize/FolderSize.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace _05.FolderSize
+{
+    public class Folder
+    {
+        public static void Main()
+        {
+            var dir = new DirectoryInfo("./TestFolder");
+            var files = dir.GetFiles();
+
+            File.WriteAllText("output.txt", files.Sum(x => x.Length / 1024.0 / 1024.0).ToString());
+        }
+    }
+}

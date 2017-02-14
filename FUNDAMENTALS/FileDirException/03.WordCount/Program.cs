62faff1bd69fdf6e51e7401197de6bdf996546bb
Rostislav <radeff@radeff.eu>
2017-02-14 18:02:47 +0200

    Tasks 03 & 04 Solved

---
+++ FUNDAMENTALS/FileDirException/03.WordCount/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03.WordCount
+{
+    public class WordCount
+    {
+        public static void Main()
+        {
+            var words = File.ReadAllText("words.txt").Split();
+            var text = File.ReadAllText("text.txt").Split($"?!, -.{Environment.NewLine}".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var result = new SortedDictionary<int, string>();
+
+            foreach (var word in words)
+            {
+                var counter = 0;
+                foreach (var w in text)
+                {
+                    if (string.Compare(w, word, true) == 0)
+                    {
+                        counter++;
+                    }
+                }
+
+                result.Add(counter, word);
+            }
+
+            File.WriteAllLines("output.txt", result
+                .OrderByDescending(x => x.Key)
+                .Select(x => string.Concat(x.Value, " - ", x.Key)));
+        }
+    }
+}
---
+++ FUNDAMENTALS/FileDirException/04.MergeFiles/MergeFiles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _04.MergeFiles
+{
+    public class MergeFiles
+    {
+        public static void Main()
+        {
+            var merged = new List<string>();
+            merged.AddRange(File.ReadAllLines("input1.txt").ToList());
+            merged.AddRange(File.ReadAllLines("input2.txt").ToList());
+
+            File.WriteAllLines("output.txt", merged.OrderBy(x => x));            
+        }
+    }
+}

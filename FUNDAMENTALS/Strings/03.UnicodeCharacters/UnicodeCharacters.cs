f340c3fb12b0dd72194ab9261d43d3c750f2eba7
Rostislav <radeff@radeff.eu>
2017-02-14 21:52:37 +0200

    Task 03 Solved

---
+++ FUNDAMENTALS/Strings/03.UnicodeCharacters/UnicodeCharacters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.UnicodeCharacters
+{
+    public class UnicodeCharacters
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().ToCharArray().Select(x => ((int)x).ToString("X4").ToLower()).ToArray();
+
+            Console.Write("\\u");       
+            Console.WriteLine(string.Join("\\u", input));
+        }
+    }
+}

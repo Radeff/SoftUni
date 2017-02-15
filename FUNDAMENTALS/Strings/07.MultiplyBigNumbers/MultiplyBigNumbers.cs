8f07ae5fb928aa8b797b7bde1492fe341eeb424a
Rostislav <radeff@radeff.eu>
2017-02-15 02:55:25 +0200

    Task 07 Solved

---
+++ FUNDAMENTALS/Strings/07.MultiplyBigNumbers/MultiplyBigNumbers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.MultiplyBigNumbers
+{
+    public class MultiplyBigNumbers
+    {
+        public static void Main()
+        {
+            var number = Console.ReadLine()
+                .TrimStart('0')
+                .ToCharArray();
+
+            var multiplier = int.Parse(Console.ReadLine());
+
+            if (multiplier == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            var result = "";
+            var mem = 0;
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                var sum = int.Parse(number[i].ToString()) * multiplier + mem;
+
+                if (sum > 9)
+                {
+                    mem = sum / 10;
+                    sum = sum % 10;
+                }
+
+                else
+                {
+                    mem = 0;
+                }
+
+                result += sum;
+            }
+
+            var reversedResult = new string(result
+                .ToCharArray()
+                .Reverse()
+                .ToArray());
+
+            if (mem != 0)
+            {
+                Console.WriteLine(mem + reversedResult);
+            }
+
+            else
+            {
+                Console.WriteLine(reversedResult);
+            }
+        }
+    }
+}

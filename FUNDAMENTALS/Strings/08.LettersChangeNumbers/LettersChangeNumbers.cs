8a818a8067b394e090df01c2e7877759d347a1b7
Rostislav <radeff@radeff.eu>
2017-02-15 03:31:03 +0200

    Task 08 Solved

---
+++ FUNDAMENTALS/Strings/08.LettersChangeNumbers/LettersChangeNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.LettersChangeNumbers
+{
+    public class LettersChangeNumbers
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(" \t".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var totalSum = 0M;
+
+            foreach (var splitItem in input)
+            {
+                var letterBefore = splitItem.ElementAt(0);
+                var number = decimal.Parse(splitItem.Substring(1, splitItem.Length - 2));
+                var letterAfter = splitItem.ElementAt(splitItem.Length - 1);
+
+                if (letterBefore >= 'A' && letterBefore <= 'Z')
+                {
+                    number /= letterBefore - 'A' + 1;
+                }
+                else
+                {
+                    number *= letterBefore - 'a' + 1;
+                }
+
+                if (letterAfter >= 'A' && letterAfter <= 'Z')
+                {
+                    number -= letterAfter - 'A' + 1;
+                }
+                else
+                {
+                    number += letterAfter - 'a' + 1;
+                }
+
+                totalSum += number;
+            }
+
+            Console.WriteLine($"{totalSum:F2}");
+        }
+    }
+}

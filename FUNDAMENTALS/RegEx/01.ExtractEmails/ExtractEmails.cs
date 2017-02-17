6e5afe332701d6f2924c15e0e02663d737e8dbea
Rostislav <radeff@radeff.eu>
2017-02-17 19:03:56 +0200

    Task 01 Solved

---
+++ FUNDAMENTALS/RegEx/01.ExtractEmails/ExtractEmails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.ExtractEmails
+{
+    public class ExtractEmails
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var pattern = new Regex(@"\b(?<!\S)[a-zA-Z0-9]+[-._]*\w*@\w+([.-]*\w)*\.\w+\b"); // (?<!\S) - Look behind!
+            var matches = pattern.Matches(input);
+
+            foreach (Match email in matches)
+            {
+                Console.WriteLine(email.Value);
+            }
+        }
+    }
+}

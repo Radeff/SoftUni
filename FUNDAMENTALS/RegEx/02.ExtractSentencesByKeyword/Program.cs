b3632d2af96014e7323c79816d8852811fa29e74
Rostislav <radeff@radeff.eu>
2017-02-17 21:13:16 +0200

    Task 02 Solved

---
+++ FUNDAMENTALS/RegEx/02.ExtractSentencesByKeyword/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.ExtractSentencesByKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keyword = Console.ReadLine();
+            var text = Console.ReadLine();
+            var pattern = new Regex($@"\b[^.!?]*{keyword}[ ,:]\w+\b[^?!.]*");
+            var matches = pattern.Matches(text);
+
+            foreach (Match sentence in matches)
+            {
+                Console.WriteLine(sentence.Value);
+            }
+        }
+    }
+}

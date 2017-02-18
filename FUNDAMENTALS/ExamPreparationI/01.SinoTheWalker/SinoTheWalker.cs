54aaf76a5150f5a0c55311fe6a3c8722457fd443
Rostislav <radeff@radeff.eu>
2017-02-18 20:55:18 +0200

    Task 01 Solved

---
+++ FUNDAMENTALS/ExamPreparationI/01.SinoTheWalker/SinoTheWalker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01.SinoTheWalker
+{
+    public class SinoTheWalker
+    {
+        public static void Main()
+        {
+            var departure = TimeSpan.ParseExact(Console.ReadLine(), "hh\\:mm\\:ss", CultureInfo.InvariantCulture);
+            var steps = int.Parse(Console.ReadLine()) % 86400;
+            var stepTime = int.Parse(Console.ReadLine()) % 86400;            
+            var timeWalking = TimeSpan.FromSeconds(stepTime * steps);
+            var arrival = departure.Add(timeWalking);
+            Console.WriteLine($"Time Arrival: {arrival.ToString("hh\\:mm\\:ss")}");            
+        }
+    }
+}

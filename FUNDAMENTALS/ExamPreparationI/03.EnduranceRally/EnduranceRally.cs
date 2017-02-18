4b7e74a3e34f143c130f11d6900fe643857c912a
Rostislav <radeff@radeff.eu>
2017-02-18 22:14:35 +0200

    Task 03 Solved

---
+++ FUNDAMENTALS/ExamPreparationI/03.EnduranceRally/EnduranceRally.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.EnduranceRally
+{
+    public class EnduranceRally
+    {
+        public static void Main()
+        {
+            var racers = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            var zones = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            var checkpoints = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            foreach (var racer in racers)
+            {
+                double fuel = racer.First();                
+
+                for (int i = 0; i < zones.Length; i++)
+                {
+                    if (checkpoints.Contains(i))
+                    {
+                        fuel += zones[i];
+                    }
+
+                    else
+                    {
+                        fuel -= zones[i];
+                    }
+
+                    if (fuel <= 0)
+                    {
+                        Console.WriteLine($"{racer} - reached {i}");
+                        break;
+                    }
+                }
+
+                if (fuel > 0)
+                {
+                    Console.WriteLine($"{racer} - fuel left {fuel:F2}");
+                }                
+            }
+        }
+    }
+}

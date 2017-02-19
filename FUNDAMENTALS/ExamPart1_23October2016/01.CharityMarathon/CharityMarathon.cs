d9de8bf616aa7737ed1f172fa3c255989b07da8e
Rostislav <radeff@radeff.eu>
2017-02-19 13:36:33 +0200

    Problem 01 Solved

---
+++ FUNDAMENTALS/ExamPart1_23October2016/01.CharityMarathon/CharityMarathon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.CharityMarathon
+{
+    public class CharityMarathon
+    {
+        public static void Main()
+        {
+            var days = int.Parse(Console.ReadLine());
+            var runners = int.Parse(Console.ReadLine());
+            var laps = int.Parse(Console.ReadLine());
+            double lapLengthKM = double.Parse(Console.ReadLine()) / 1000;
+            var capacity = int.Parse(Console.ReadLine());
+            var moneyPerKm = double.Parse(Console.ReadLine());
+
+            if (runners > days * capacity)
+            {
+                runners = days * capacity;
+            }
+
+            double totalDistance = runners * laps * lapLengthKM;
+            double totalRaised = totalDistance * moneyPerKm;
+            Console.WriteLine($"Money raised: {totalRaised:F2}");
+        }
+    }
+}

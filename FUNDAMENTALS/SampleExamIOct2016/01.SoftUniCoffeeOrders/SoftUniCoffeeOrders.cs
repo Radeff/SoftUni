e185dabe68816f7a9a56176a53f2caded8be1ada
Rostislav <radeff@radeff.eu>
2017-02-20 15:44:54 +0200

    Problem 01 Solved

---
+++ FUNDAMENTALS/SampleExamIOct2016/01.SoftUniCoffeeOrders/SoftUniCoffeeOrders.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01.SoftUniCoffeeOrders
+{
+    public class SoftUniCoffeeOrders
+    {
+        public static void Main()
+        {
+            var orders = int.Parse(Console.ReadLine());
+            var total = 0M;
+
+            for (int i = 0; i < orders; i++)
+            {
+                decimal price = decimal.Parse(Console.ReadLine());
+                var date = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+                var capsules = long.Parse(Console.ReadLine());
+                var daysInMonth = DateTime.DaysInMonth(date.Year, date.Month);
+                var pricePerMonth = daysInMonth * capsules * price;
+                total += pricePerMonth;
+
+                Console.WriteLine($"The price for the coffee is: ${pricePerMonth:F2}");
+            }
+
+            Console.WriteLine($"Total: ${total:F2}");
+        }
+    }
+}

ba5e7316e866074f6496537f6eae2910be2e28be
Rostislav <radeff@radeff.eu>
2017-02-25 13:25:34 +0200

    Problem 01 Solved

---
+++ FUNDAMENTALS/Exam12Jun2016/01.Numbers/Numbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Numbers
+{
+    public class Numbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var average = numbers.Average();
+            var result = new List<int>();
+
+            foreach (var number in numbers)
+            {
+                if (number > average)
+                {
+                    result.Add(number);
+                }
+            }
+
+            var count = 5;
+
+            if (count > result.Count)
+            {
+                count = result.Count;
+            }
+
+            if (result.Any())
+            {
+                Console.WriteLine($"{string.Join(" ", result.OrderByDescending(n => n).Take(count))}");
+            }
+
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}

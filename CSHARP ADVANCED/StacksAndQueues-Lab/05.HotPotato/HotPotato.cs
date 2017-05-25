3b92083fe73f4dd01d275a3fc7a1722953597137
Rostislav <radeff@radeff.eu>
2017-05-25 18:38:54 +0300

    Problem 05 - Solved

---
+++ CSHARP ADVANCED/StacksAndQueues-Lab/05.HotPotato/HotPotato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.HotPotato
+{
+	public class HotPotato
+	{
+		public static void Main()
+		{
+			var input = Console.ReadLine().Split();
+			var queue = new Queue<string>(input);
+			var n = int.Parse(Console.ReadLine());
+
+			while (queue.Count > 1)
+			{
+				for (int i = 0; i < n - 1; i++)
+				{
+					var survivor = queue.Dequeue();
+					queue.Enqueue(survivor);
+				}
+
+				Console.WriteLine($"Removed {queue.Dequeue()}");
+			}
+
+			Console.WriteLine($"Last is {queue.Peek()}");
+		}
+	}
+}

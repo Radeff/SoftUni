8007fe1d489f1166cb8e6690b4b084786faa2cf6
Rostislav <radeff@radeff.eu>
2017-05-25 17:29:08 +0300

    Task 1 - Solved

---
+++ CSHARP ADVANCED/StacksAndQueues-Lab/1.ReverseStrings/ReverseStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.ReverseStrings
+{
+	public class ReverseStrings
+	{
+		public static void Main()
+		{
+			var input = Console.ReadLine();
+			var stack = new Stack<char>();
+
+			foreach (var c in input)
+			{
+				stack.Push(c);
+			}
+
+			for (var i = 0; i < input.Length; i++)
+			{
+				Console.Write(stack.Pop());
+			}
+
+			Console.WriteLine();
+		}
+	}
+}

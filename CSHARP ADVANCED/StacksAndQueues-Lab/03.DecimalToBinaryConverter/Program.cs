b501b6f4db25b2b9add1310d9e0b490af396d7e2
Rostislav <radeff@radeff.eu>
2017-05-25 18:00:31 +0300

    Problem 03 - Solved

---
+++ CSHARP ADVANCED/StacksAndQueues-Lab/03.DecimalToBinaryConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.DecimalToBinaryConverter
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			var numToConvert = int.Parse(Console.ReadLine());
+			var stack = new Stack<int>();
+
+			if (numToConvert == 0)
+			{
+				Console.WriteLine(0);
+			}
+			else
+			{
+				while (numToConvert != 0)
+				{
+					stack.Push(numToConvert % 2);
+					numToConvert /= 2;
+				}
+			}
+
+			while (stack.Count > 0)
+			{
+				Console.Write(stack.Pop());
+			}
+
+			Console.WriteLine();
+		}
+	}
+}

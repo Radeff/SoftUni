de4e4a6106d06e41b408a3dfe2797fe178c5cedd
Rostislav <radeff@radeff.eu>
2017-05-25 18:23:32 +0300

    Problem 04 - Solved

---
+++ CSHARP ADVANCED/StacksAndQueues-Lab/04.MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.MatchingBrackets
+{
+	public class MatchingBrackets
+	{
+		public static void Main()
+		{
+			var input = Console.ReadLine();
+			var stack = new Stack<int>();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (input[i] == '(')
+				{
+					stack.Push(i);
+				}
+
+				if (input[i] == ')')
+				{
+					var start = stack.Pop();
+					var result = input.Substring(start, i - start + 1);
+					Console.WriteLine(result);
+				}
+			}
+		}
+	}
+}

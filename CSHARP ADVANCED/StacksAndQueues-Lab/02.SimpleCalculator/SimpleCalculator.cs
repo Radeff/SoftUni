1b1f7e4f82d85b3963a62a20a50a3fb5077ad960
Rostislav <radeff@radeff.eu>
2017-05-25 17:49:02 +0300

    Task 02 - Solved

---
+++ CSHARP ADVANCED/StacksAndQueues-Lab/02.SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SimpleCalculator
+{
+	public class SimpleCalculator
+	{
+		public static void Main()
+		{
+			var input = Console.ReadLine().Split().Reverse();
+			var stack = new Stack<string>();
+
+			foreach (var item in input)
+			{
+				stack.Push(item);
+			}
+
+			while (stack.Count > 1)
+			{
+				var firstNum = int.Parse(stack.Pop());
+				var oper = stack.Pop();
+				var secondNum = int.Parse(stack.Pop());
+
+				if (oper == "+")
+				{
+					stack.Push((firstNum + secondNum).ToString());
+				}
+				else
+				{
+					stack.Push((firstNum - secondNum).ToString());
+				}
+			}
+
+			Console.WriteLine(stack.Pop());
+		}
+	}
+}

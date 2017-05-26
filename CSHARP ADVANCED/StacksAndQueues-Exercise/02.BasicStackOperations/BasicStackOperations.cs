7046cab6388a8e4cfbe40b57861e428af72f7fce
Rostislav <radeff@radeff.eu>
2017-05-26 20:32:42 +0300

    Problem 02 - Solved

---
+++ CSHARP ADVANCED/StacksAndQueues-Exercise/02.BasicStackOperations/BasicStackOperations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.BasicStackOperations
+{
+    public class BasicStackOperations
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var n = input[0];
+            var s = input[1];
+            var x = input[2];
+
+            var numbers = Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            foreach (var number in stack)
+            {
+                if (number == x)
+                {
+                    Console.WriteLine("true");
+                    return;
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}

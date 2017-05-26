a602aeff63c9f4303a7862413c845a25a148fb47
Rostislav <radeff@radeff.eu>
2017-05-26 21:02:56 +0300

    Problem 04 - Solved

---
+++ CSHARP ADVANCED/StacksAndQueues-Exercise/04.BasicQueueOperations/BasicQueueOperations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.BasicQueueOperations
+{
+    public class BasicQueueOperations
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var n = input[0];
+            var s = input[1];
+            var x = input[2];
+
+            var numbers = Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            foreach (var number in queue)
+            {
+                if (number == x)
+                {
+                    Console.WriteLine("true");
+                    return;
+                }
+            }
+
+            if (queue.Count > 0)
+            {
+                Console.WriteLine(queue.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}

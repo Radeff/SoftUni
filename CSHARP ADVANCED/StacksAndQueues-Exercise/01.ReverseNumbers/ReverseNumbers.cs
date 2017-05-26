228f9a4bbd2265cc45d91cd7df225f7906ee6c1e
Rostislav <radeff@radeff.eu>
2017-05-26 20:13:31 +0300

    Problem 01 - Solved

---
+++ CSHARP ADVANCED/StacksAndQueues-Exercise/01.ReverseNumbers/ReverseNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ReverseNumbers
+{
+    public class ReverseNumbers
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var stack = new Stack<int>();
+
+            foreach (var number in input)
+            {
+                stack.Push(number);
+            }
+
+            while (stack.Count > 1)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}

8a14bc4832cfe5bb011b39dfa1a86dd00254fc8b
Rostislav <radeff@radeff.eu>
2017-05-27 18:59:32 +0300

    Problem 09 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/StacksAndQueues-Exercise/09.StackFibonacci/StackFibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09.StackFibonacci
+{
+    public class StackFibonacci
+    {
+        public static void Main()
+        {
+            var nthNumber = int.Parse(Console.ReadLine());
+            var result = GetFibonacci(nthNumber);
+            Console.WriteLine(result);
+        }
+
+        private static long GetFibonacci(int nthNumber)
+        {
+            var fibStack = new Stack<long>();
+            fibStack.Push(0);
+            fibStack.Push(1);
+
+            for (int i = 0; i < nthNumber - 1; i++)
+            {
+                var current = fibStack.Pop();
+                var previous = fibStack.Pop();
+                var next = previous + current;
+                fibStack.Push(current);
+                fibStack.Push(next);
+            }
+
+            return fibStack.Peek();
+        }
+    }
+}

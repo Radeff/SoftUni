0445f5575a56f5bbd02c77a94e2bf4f2858d5bba
Rostislav <radeff@radeff.eu>
2017-05-27 16:15:01 +0300

    Problem 06 - Solved & Uploaded Tasks Doc

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/StacksAndQueues-Exercise/06.TruckTour/TruckTour.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.TruckTour
+{
+    public class TruckTour
+    {
+        public static void Main()
+        {
+            var pumpsNumber = int.Parse(Console.ReadLine());
+            var circle = new Queue<long[]>();
+
+            for (int i = 0; i < pumpsNumber; i++)
+            {
+                circle.Enqueue(Console.ReadLine().Split().Select(long.Parse).ToArray());
+            }
+
+            var possible = false;
+
+            for (int i = 0; i < pumpsNumber; i++)
+            {
+                long fuel = 0;
+
+                foreach (var pump in circle)
+                {
+                    fuel += pump[0];
+                    fuel -= pump[1];
+
+                    if (fuel >= 0)
+                    {
+                        possible = true;
+                    }
+                    else
+                    {
+                        possible = false;
+                        break;
+                    }
+                }
+
+                if (possible)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+
+                var pumpToMove = circle.Dequeue();
+                circle.Enqueue(pumpToMove);
+            }
+        }
+    }
+}

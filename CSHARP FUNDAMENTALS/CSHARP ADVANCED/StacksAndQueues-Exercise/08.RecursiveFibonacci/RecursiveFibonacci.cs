9588152a4822cc1f203df31dc86b94107b68542b
Rostislav <radeff@radeff.eu>
2017-05-27 18:45:49 +0300

    Problem 08 - Solved (Copied from lector)

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/StacksAndQueues-Exercise/08.RecursiveFibonacci/RecursiveFibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.RecursiveFibonacci
+{
+    public class RecursiveFibonacci
+    {
+        private static long[] fibNumbers;
+        public static void Main()
+        {
+            var nthNumber = int.Parse(Console.ReadLine());
+            fibNumbers = new long[nthNumber];
+            var result = GetFibonacci(nthNumber);
+            Console.WriteLine(result);
+        }
+
+        private static long GetFibonacci(int nthNumber)
+        {
+            if (nthNumber <= 2)
+            {
+                return 1;
+            }
+            if (fibNumbers[nthNumber - 1] != 0)
+            {
+                return fibNumbers[nthNumber - 1];
+            }
+
+            return fibNumbers[nthNumber - 1] = GetFibonacci(nthNumber - 1) + GetFibonacci(nthNumber - 2);
+        }
+    }
+}

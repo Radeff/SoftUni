94faa991ecc677a8edac605987c48b5201d81a99
Rostislav <radeff@radeff.eu>
2017-05-27 17:55:23 +0300

    Problem 07 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/StacksAndQueues-Exercise/07.BalancedParenthesis/BalancedParenthesis.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.BalancedParenthesis
+{
+    public class BalancedParenthesis
+    {
+        public static void Main()
+        {
+            var brackets = Console.ReadLine();
+            var opened = new char[] {'(', '[', '{'};
+            var openBrackets = new Stack<char>();
+
+            foreach (var bracket in brackets)
+            {
+                if (opened.Contains(bracket))
+                {
+                    openBrackets.Push(bracket);
+                }
+                else
+                {
+                    if (openBrackets.Count == 0)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                    switch (bracket)
+                    {
+                        case ')':
+                            if (openBrackets.Pop() != '(')
+                            {
+                                Console.WriteLine("NO");
+                                return;
+                            }
+                            break;
+
+                        case ']':
+                            if (openBrackets.Pop() != '[')
+                            {
+                                Console.WriteLine("NO");
+                                return;
+                            }
+                            break;
+
+                        case '}':
+                            if (openBrackets.Pop() != '{')
+                            {
+                                Console.WriteLine("NO");
+                                return;
+                            }
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine("YES");
+        }
+    }
+}

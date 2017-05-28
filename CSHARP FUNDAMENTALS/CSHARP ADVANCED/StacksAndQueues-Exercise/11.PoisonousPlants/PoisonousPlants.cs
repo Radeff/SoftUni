593afae7ad9f052936560768e9917ce2b55ded8c
Rostislav <radeff@radeff.eu>
2017-05-28 17:20:51 +0300

    Problem 11 - Solved (Copied from lector)

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/StacksAndQueues-Exercise/11.PoisonousPlants/PoisonousPlants.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.PoisonousPlants
+{
+    public class PoisonousPlants
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var plants = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var days = new int[plants.Length];
+            var indexes = new Stack<int>();
+            indexes.Push(0);
+
+            for (int i = 1; i < plants.Length; i++)
+            {
+                var maxDaysAlive = 0;
+
+                while (indexes.Count > 0 && plants[indexes.Peek()] >= plants[i])
+                {
+                    maxDaysAlive = Math.Max(maxDaysAlive, days[indexes.Pop()]);
+                }
+
+                if (indexes.Count > 0)
+                {
+                    days[i] = maxDaysAlive + 1;
+                }
+                indexes.Push(i);
+            }
+            Console.WriteLine(days.Max());
+        }
+    }
+}

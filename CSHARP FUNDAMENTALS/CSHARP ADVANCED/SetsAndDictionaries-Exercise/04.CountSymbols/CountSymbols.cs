2c95313e8b682632ffa60284c7f9d91edfa63b88
Rostislav <radeff@radeff.eu>
2017-05-29 21:33:44 +0300

    Problem 04 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/SetsAndDictionaries-Exercise/04.CountSymbols/CountSymbols.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.CountSymbols
+{
+    public class CountSymbols
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var text = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!text.ContainsKey(input[i]))
+                {
+                    text.Add(input[i], 1);
+                }
+                else
+                {
+                    text[input[i]]++;
+                }
+            }
+
+            foreach (var kvp in text)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}

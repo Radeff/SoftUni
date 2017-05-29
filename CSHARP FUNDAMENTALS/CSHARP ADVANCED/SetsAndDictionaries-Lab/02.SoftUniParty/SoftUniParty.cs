b75d74728f6b0e1af9d0eba99b2adcd760622582
Rostislav <radeff@radeff.eu>
2017-05-29 17:51:36 +0300

    Problem 02 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/SetsAndDictionaries-Lab/02.SoftUniParty/SoftUniParty.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.SoftUniParty
+{
+    public class SoftUniParty
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var guests = new SortedSet<string>();
+
+            while (input != "PARTY")
+            {
+                guests.Add(input);
+                input = Console.ReadLine();
+            }
+
+            while (input != "END")
+            {
+                guests.Remove(input);
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(guests.Count);
+            foreach (var guest in guests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}

010002a171463cfc3fe9c79299083156a6fce3ef
Rostislav <radeff@radeff.eu>
2017-05-29 21:26:44 +0300

    Problem 03 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/SetsAndDictionaries-Exercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.PeriodicTable
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var set = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                   set.Add(input[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", set));
+        }
+    }
+}

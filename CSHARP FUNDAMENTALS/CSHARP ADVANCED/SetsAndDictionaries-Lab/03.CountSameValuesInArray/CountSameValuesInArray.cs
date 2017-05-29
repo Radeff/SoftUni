7f2e1560f4d574fafa01695acb57fbb01efbaef5
Rostislav <radeff@radeff.eu>
2017-05-29 18:56:09 +0300

    Problem 03 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/SetsAndDictionaries-Lab/03.CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.CountSameValuesInArray
+{
+    public class CountSameValuesInArray
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse);
+            var dict = new SortedDictionary<double, int>();
+
+            foreach (var num in numbers)
+            {
+                if (!dict.ContainsKey(num))
+                {
+                    dict.Add(num, 1);
+                }
+                else
+                {
+                    dict[num]++;
+                }
+            }
+
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}

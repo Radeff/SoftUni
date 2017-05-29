4981c5562704fbbe14078cdc6848cd654843bdc4
Rostislav <radeff@radeff.eu>
2017-05-29 22:12:49 +0300

    Problem 05 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/SetsAndDictionaries-Exercise/05.Phonebook/Phonebook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.Phonebook
+{
+    public class Phonebook
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split('-').ToArray();
+            var phonebook = new Dictionary<string, string>();
+
+            while (input[0] != "search")
+            {
+                var name = input[0];
+                var number = input[1];
+
+                if (!phonebook.ContainsKey(name))
+                {
+                    phonebook.Add(name, number);
+                }
+                else
+                {
+                    phonebook[name] = number;
+                }
+
+                input = Console.ReadLine().Split('-').ToArray();
+            }
+
+            var nameSearch = Console.ReadLine();
+
+            while (nameSearch != "stop")
+            {
+                if (phonebook.ContainsKey(nameSearch))
+                {
+                    Console.WriteLine($"{nameSearch} -> {phonebook[nameSearch]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Contact {nameSearch} does not exist.");
+                }
+
+                nameSearch = Console.ReadLine();
+            }
+        }
+    }
+}

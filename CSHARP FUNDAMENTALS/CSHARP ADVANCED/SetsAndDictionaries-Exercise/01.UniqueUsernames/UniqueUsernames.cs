608968d24ecbbcf172b70a04f26bf89fe0df1a69
Rostislav <radeff@radeff.eu>
2017-05-29 20:55:34 +0300

    Problem 01 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/SetsAndDictionaries-Exercise/01.UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUsernames
+{
+    public class UniqueUsernames
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                set.Add(Console.ReadLine());
+            }
+
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}

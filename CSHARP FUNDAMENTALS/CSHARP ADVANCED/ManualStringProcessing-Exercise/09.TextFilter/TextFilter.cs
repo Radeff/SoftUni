bc0faeae04611cb2c48a8fcab19999ed8643601a
Rostislav <radeff@radeff.eu>
2017-06-10 18:50:09 +0300

    Problem 10 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/ManualStringProcessing-Exercise/09.TextFilter/TextFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09.TextFilter
+{
+    public class TextFilter
+    {
+        public static void Main()
+        {
+            var banlist = Console.ReadLine()
+                .Split(", ".ToCharArray(),StringSplitOptions.RemoveEmptyEntries);
+            var text = Console.ReadLine();
+
+            foreach (var bannedWord in banlist)
+            {
+                text = text.Replace(bannedWord, new string('*', bannedWord.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}

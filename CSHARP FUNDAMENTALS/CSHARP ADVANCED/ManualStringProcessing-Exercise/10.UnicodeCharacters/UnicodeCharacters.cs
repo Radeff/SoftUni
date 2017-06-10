0a1afa4828d9f727e2d15d7330f9dd50b1debbc9
Rostislav <radeff@radeff.eu>
2017-06-10 18:59:40 +0300

    Problem 10 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/ManualStringProcessing-Exercise/10.UnicodeCharacters/UnicodeCharacters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.UnicodeCharacters
+{
+    public class UnicodeCharacters
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .ToCharArray()
+                .Select(x => ((int)x).ToString("X4")
+                .ToLower())
+                .ToArray();
+
+            Console.Write("\\u");
+            Console.WriteLine(string.Join("\\u", input));
+        }
+    }
+}

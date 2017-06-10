21c18cba0800f5e00452cad452bfeb1d72874263
Rostislav <radeff@radeff.eu>
2017-06-10 16:52:54 +0300

    Problem 01 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/ManualStringProcessing-Exercise/01.ReverseString/ReverseString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ReverseString
+{
+    public class ReverseString
+    {
+        public static void Main()
+        {
+            var toReverse = Console.ReadLine();
+            var reversed = toReverse.ToCharArray();
+            Array.Reverse(reversed);
+            Console.WriteLine(reversed.ToArray());
+        }
+    }
+}

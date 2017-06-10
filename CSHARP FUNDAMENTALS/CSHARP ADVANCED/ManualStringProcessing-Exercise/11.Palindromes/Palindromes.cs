3b9a3cff6e1d8bae9c727a8a7c201f3a0d53a72d
Rostislav <radeff@radeff.eu>
2017-06-10 19:16:24 +0300

    Problem 11 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/ManualStringProcessing-Exercise/11.Palindromes/Palindromes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11.Palindromes
+{
+    public class Palindromes
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine().Split("., !?".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var uniques = new SortedSet<string>();
+
+            foreach (var word in text)
+            {
+                var start = word.Substring(0, word.Length / 2);
+                var end = start.ToCharArray();
+                Array.Reverse(end);
+
+                if (word.EndsWith(new string(end)))
+                {
+                    uniques.Add(word);
+                }
+            }
+
+            Console.Write("[");
+            Console.Write(string.Join(", ", uniques));
+            Console.WriteLine("]");
+        }
+    }
+}

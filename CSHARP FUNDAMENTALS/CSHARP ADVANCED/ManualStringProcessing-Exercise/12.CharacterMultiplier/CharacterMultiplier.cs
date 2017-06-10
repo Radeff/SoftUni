5e0a5dddedd3b7adec73b4fb762d98c461ebd6e0
Rostislav <radeff@radeff.eu>
2017-06-10 19:23:08 +0300

    Problem 12 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/ManualStringProcessing-Exercise/12.CharacterMultiplier/CharacterMultiplier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _12.CharacterMultiplier
+{
+    public class CharacterMultiplier
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            var first = input[0].ToCharArray();
+            var second = input[1].ToCharArray();
+            var shorter = first.Length;
+            var longer = second.Length;
+            var diff = Math.Abs(first.Length - second.Length);
+
+            if (first.Length > second.Length)
+            {
+                shorter = second.Length;
+                longer = first.Length;
+            }
+
+            var sum = 0;
+
+            for (int i = 0; i < shorter; i++)
+            {
+                sum += first[i] * second[i];
+            }
+
+            if (diff != 0)
+            {
+                for (int i = longer - diff; i < longer; i++)
+                {
+                    if (longer == first.Length)
+                    {
+                        sum += first[i];
+                    }
+                    else
+                    {
+                        sum += second[i];
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}

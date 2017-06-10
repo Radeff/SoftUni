c08995905cf0a5eef174398c94d7f50e470097e4
Rostislav <radeff@radeff.eu>
2017-06-10 17:47:19 +0300

    Problem 05 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/ManualStringProcessing-Exercise/05.ConvertFromBaseNToBase10/ConvertFromBaseNToBase10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05.ConvertFromBaseNToBase10
+{
+    public class ConvertFromBaseNToBase10
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            var n = int.Parse(input[0]);
+            var number = BigInteger.Parse(input[1]);
+            BigInteger result = 0;
+            int power = 0;
+
+            while (number > 0)
+            {
+                result += (number % 10) * BigInteger.Pow(n, power);
+                number /= 10;
+                power++;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

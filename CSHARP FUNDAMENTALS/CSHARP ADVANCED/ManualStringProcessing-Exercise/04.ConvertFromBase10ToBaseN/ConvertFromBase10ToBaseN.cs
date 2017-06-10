32e95c0c0baaccbc5adb6fa0a161303651d0af16
Rostislav <radeff@radeff.eu>
2017-06-10 17:43:39 +0300

    Problem 04 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/ManualStringProcessing-Exercise/04.ConvertFromBase10ToBaseN/ConvertFromBase10ToBaseN.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace _04.ConvertFromBase10ToBaseN
+{
+    class ConvertFromBase10ToBaseN
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split().Select(BigInteger.Parse).ToArray();
+            var n = input[0];
+            var number = input[1];
+            var sb = new StringBuilder();
+
+            while (number > 0)
+            {
+                sb.Append(number % n);
+                number = number / n;
+            }
+
+            var toReverse = sb.ToString().ToCharArray();
+            Array.Reverse(toReverse);
+            var result = toReverse.ToArray();
+            Console.WriteLine(result);
+        }
+    }
+}

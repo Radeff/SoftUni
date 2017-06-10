fac1a65ae2611f967c5993cc9872f187a7acad39
Rostislav <radeff@radeff.eu>
2017-06-10 18:23:41 +0300

    Problem 06 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/ManualStringProcessing-Exercise/06.CountSubstringOccurances/CountSubstringOccurances.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06.CountSubstringOccurances
+{
+    public class CountSubstringOccurances
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine().ToLower();
+            var pattern = Console.ReadLine().ToLower();
+            var counter = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var textCut = text.Substring(i, text.Length - i);
+
+                if (textCut.Contains(pattern))
+                {
+                    i += textCut.IndexOf(pattern);
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}

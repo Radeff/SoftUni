357d7707382b1cce8d6be85f9dc672a8e234a7f0
Rostislav <radeff@radeff.eu>
2017-06-13 01:41:56 +0300

    Problem 07 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Lab/07.ValidTime/ValidTime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _07.ValidTime
+{
+    public class ValidTime
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var regex = new Regex(@"^(0[0-9]|1[0-2])(:[0-5][0-9]){2}\s[AP]M$");
+
+            while (input != "END")
+            {
+                var match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    Console.WriteLine("valid");
+                }
+                else
+                {
+                    Console.WriteLine("invalid");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}

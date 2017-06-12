491ab1aa507cbd36350b66eb33ae732a81d41396
Rostislav <radeff@radeff.eu>
2017-06-13 00:12:41 +0300

    Problem 05 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Lab/05.ExtractTags/ExtractTags.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _05.ExtractTags
+{
+    public class ExtractTags
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var regex = new Regex("<.+?>");
+
+            while (input != "END")
+            {
+                var matches = regex.Matches(input);
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}

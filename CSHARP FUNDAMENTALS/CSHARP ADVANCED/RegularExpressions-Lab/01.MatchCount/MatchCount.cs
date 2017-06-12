845b4e73c9b8028fc8acf0d9b4317cd24c52be9d
Rostislav <radeff@radeff.eu>
2017-06-12 23:50:32 +0300

    Problem 01 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Lab/01.MatchCount/MatchCount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchCount
+{
+    public class MatchCount
+    {
+        public static void Main()
+        {
+            var search = Console.ReadLine();
+            var text = Console.ReadLine();
+            var regex = new Regex(search);
+            var matches = regex.Matches(text);
+
+            Console.WriteLine(matches.Count);
+        }
+    }
+}

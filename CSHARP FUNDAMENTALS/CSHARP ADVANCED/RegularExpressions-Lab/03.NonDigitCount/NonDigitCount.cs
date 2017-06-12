6b5effb9f9183c573f534e043fd42a348c620b92
Rostislav <radeff@radeff.eu>
2017-06-13 00:00:27 +0300

    Problem 03 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Lab/03.NonDigitCount/NonDigitCount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03.NonDigitCount
+{
+    public class NonDigitCount
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var regex = new Regex(@"\D");
+            Console.WriteLine($"Non-digits: {regex.Matches(text).Count}");
+        }
+    }
+}

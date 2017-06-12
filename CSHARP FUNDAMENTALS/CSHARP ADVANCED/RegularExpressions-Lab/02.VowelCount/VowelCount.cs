13d40d7866a7518ae6fb3dcc5f67a47d3ac094d4
Rostislav <radeff@radeff.eu>
2017-06-12 23:54:59 +0300

    Problem 02 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Lab/02.VowelCount/VowelCount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.VowelCount
+{
+    public class VowelCount
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var vowels = "[aeiouyAEIOUY]";
+            var regex = new Regex(vowels);
+            Console.WriteLine($"Vowels: {regex.Matches(text).Count}");
+        }
+    }
+}

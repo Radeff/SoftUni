20cd87eccb3f2af936439b2ce218d1a230a2b7a1
Rostislav <radeff@radeff.eu>
2017-06-13 00:06:03 +0300

    Problem 04 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Lab/04.ExtractIntegerNumbers/ExtractIntegerNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _04.ExtractIntegerNumbers
+{
+    public class ExtractIntegerNumbers
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var regex = new Regex(@"\d+");
+            var matches = regex.Matches(text);
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+            }
+        }
+    }
+}

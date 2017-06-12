64c4b211475fcc5a82b445147431fa292ba8f622
Rostislav <radeff@radeff.eu>
2017-06-13 02:51:48 +0300

    Problem 08 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Lab/08.ExtractQuotations/ExtractQuotations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _08.ExtractQuotations
+{
+    public class ExtractQuotations
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var regex = new Regex(@"('|"")(.+?)\1");
+            var matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[2].Value);
+            }
+        }
+    }
+}

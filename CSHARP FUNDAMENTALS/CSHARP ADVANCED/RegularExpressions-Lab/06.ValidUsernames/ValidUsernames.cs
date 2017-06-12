08bf4dda40fe559b63d8ab33f2732161984857b6
Rostislav <radeff@radeff.eu>
2017-06-13 00:20:37 +0300

    Problem 06 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Lab/06.ValidUsernames/ValidUsernames.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.ValidUsernames
+{
+    public class ValidUsernames
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var regex = new Regex(@"^[-\w]{3,16}$");
+
+            while (input != "END")
+            {
+                if (regex.Match(input).Success)
+                {
+                    Console.WriteLine("valid");
+                }
+                else
+                {
+                    Console.WriteLine("invalid");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}

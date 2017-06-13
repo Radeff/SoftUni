7d2392424b22c843db74b26c42b65a5f0317fbda
Rostislav <radeff@radeff.eu>
2017-06-13 18:27:07 +0300

    Problem 01 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Exercise/01.MatchFullName/MatchFullName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    public class MatchFullName
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var regex = new Regex(@"\b[A-Z]{1}[a-z]+\b\ \b[A-Z]{1}[a-z]+\b");
+
+            while (input != "end")
+            {
+                var match = regex.Match(input);
+                if (match.Success)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}

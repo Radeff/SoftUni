152640264b689dbb6783603f09648a098f6b7e7f
Rostislav <radeff@radeff.eu>
2017-06-13 18:34:07 +0300

    Problem 02 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Exercise/02.MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace _02.MatchPhoneNumber
+{
+    public class MatchPhoneNumber
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var regex = new Regex(@"^\+359([- ])2\1\d{3}\1\d{4}$");
+
+            while (input != "end")
+            {
+                var match = regex.Match(input);
+                if (match.Success)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}

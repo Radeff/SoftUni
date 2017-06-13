27307b0637fb9c6cf69951a801fbf8316b2972b4
Rostislav <radeff@radeff.eu>
2017-06-13 20:24:21 +0300

    Problem 05 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Exercise/05.ExtractEmails/ExtractEmails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _05.ExtractEmails
+{
+    public class ExtractEmails
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var regex = new Regex(@"\s([A-Za-z0-9]+[._-]?[A-Za-z0-9]+@[A-Za-z]+[-]?[A-Za-z]+\.[A-Za-z]+\.?[A-Za-z]+)");
+
+            var matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[1].Value);
+            }
+        }
+    }
+}

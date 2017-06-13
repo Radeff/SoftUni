453ade1b89802aadd53ce70800e812197cc2bba3
Rostislav <radeff@radeff.eu>
2017-06-13 19:41:44 +0300

    Problem 04 - Sovled

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Exercise/04.ReplaceATag/ReplaceATag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _04.ReplaceATag
+{
+    public class ReplaceATag
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var regex = new Regex(@"<a(.*?href.*?=.*?)>(.*?)<\/a>");
+            //gr1 - href, gr2 - tag content, 
+            while (input != "end")
+            {
+                var replacement = "[URL$1]$2[/URL]";
+                var result = regex.Replace(input, replacement);
+                Console.WriteLine(result);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}

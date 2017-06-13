a07804b39c64b3ee7a32d685b3a2f840028804a3
Rostislav <radeff@radeff.eu>
2017-06-13 21:31:19 +0300

    Problem 06 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Exercise/06.SentenceExtractor/SentenceExtractor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.SentenceExtractor
+{
+    public class SentenceExtractor
+    {
+        public static void Main()
+        {
+            var keyword = Console.ReadLine();
+            var text = Console.ReadLine();
+            var regex = new Regex($@"[^.!?]+?\b{keyword}\b[^.!?]+?[!?.]");
+            var matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}

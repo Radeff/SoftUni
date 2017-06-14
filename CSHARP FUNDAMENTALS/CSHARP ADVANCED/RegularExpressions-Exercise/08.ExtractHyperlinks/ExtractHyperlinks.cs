5ce637d176497a167746bb5ae138a7a40372b5b8
Rostislav <radeff@radeff.eu>
2017-06-14 15:06:55 +0300

    Problems 08 & 09 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Exercise/08.ExtractHyperlinks/ExtractHyperlinks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _08.ExtractHyperlinks
+{
+    public class ExtractHyperlinks
+    {
+        public static void Main()
+        {
+            var html = new StringBuilder();
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                html.Append(input);
+                input = Console.ReadLine();
+            }
+            
+            var regex = new Regex(@"<a\s+(?:[^>]+\s+)?href\s*=\s*(?:'([^']*)'|""([^""]*)""|([^\s>]+))[^>]*>");
+            var matches = regex.Matches(html.ToString());
+
+            foreach (Match match in matches)
+            {
+                for (int i = 1; i <= 3; i++)
+                {
+                    if (match.Groups[i].Length > 0)
+                    {
+                        Console.WriteLine(match.Groups[i]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/RegularExpressions-Exercise/09.QueryMess/QueryMess.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _09.QueryMess
+{
+    public class QueryMess
+    {
+        public static void Main()
+        {
+            var input = string.Empty;
+
+            while (input != "END")
+            {
+                input = Console.ReadLine();
+                var keyValue = @"([^&?]*)=([^&?]*)";
+                var plus = @"(\+)+";
+                var space = @"(%20)+";
+                var result = new Dictionary<string, List<string>>();
+                var matches = Regex.Matches(input, keyValue);
+
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    var key = matches[i].Groups[1].Value;
+                    key = Regex.Replace(key, plus, " ").Trim();
+                    key = Regex.Replace(key, space, " ").Trim();
+                    key = Regex.Replace(key, @"(\s)+", " ");
+
+                    var value = matches[i].Groups[2].Value;
+                    value = Regex.Replace(value, plus, " ").Trim();
+                    value = Regex.Replace(value, space, " ").Trim();
+                    value = Regex.Replace(value, @"(\s)+", " ");
+
+                    if (!result.ContainsKey(key))
+                    {
+                        result.Add(key, new List<string>());
+                    }
+
+                    result[key].Add(value);
+                }
+
+                foreach (var kvp in result)
+                {
+                    Console.Write($"{kvp.Key}=[{string.Join(", ", kvp.Value)}]");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}

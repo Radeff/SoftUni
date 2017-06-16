68239f861966086f6435016f9f745e91a716b6c2
Rostislav <radeff@radeff.eu>
2017-06-16 22:38:18 +0300

    Problem 01 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Lab/01.SortEvenNumbers/SortEvenNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    public class SortEvenNumbers
+    {
+        public static void Main()
+        {
+            Func<string, int> NumberParser = n => int.Parse(n);
+            var numbers = Console.ReadLine()
+                .Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(NumberParser);
+
+            Func<int, bool> EvenSelector = n => n % 2 == 0;
+            Console.WriteLine(string.Join(", ", numbers.Where(EvenSelector).OrderBy(n => n)));
+        }
+    }
+}

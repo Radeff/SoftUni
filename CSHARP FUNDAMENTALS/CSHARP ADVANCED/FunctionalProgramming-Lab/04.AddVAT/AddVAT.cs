f44db737fff7e1c96053ace8f709c5b328a0f65f
Rostislav <radeff@radeff.eu>
2017-06-16 23:08:10 +0300

    Problem 04 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Lab/04.AddVAT/AddVAT.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    public class AddVAT
+    {
+        public static void Main()
+        {
+            Func<string, double> NumberParser = n => double.Parse(n);
+            Func<double, double> AddVat = n => n * 1.2;
+            var numbers = Console.ReadLine()
+                .Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(NumberParser)
+                .Select(AddVat);
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:F2}");
+            }
+        }
+    }
+}

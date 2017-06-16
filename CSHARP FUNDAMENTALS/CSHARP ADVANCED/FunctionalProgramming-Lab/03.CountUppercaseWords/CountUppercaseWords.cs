3ce676d1627fa9b033b53ad9b67b2586e9426bd8
Rostislav <radeff@radeff.eu>
2017-06-16 23:01:16 +0300

    Problem 03 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Lab/03.CountUppercaseWords/CountUppercaseWords.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    public class CountUppercaseWords
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine().Split();
+            Func<string, bool> FirstUpperCase = s => s.Length > 0 && (s[0] >= 'A' && s[0] <= 'Z');
+
+            var UpperCaseWords = words.Where(w => FirstUpperCase(w));
+            Console.WriteLine(string.Join("\r\n", UpperCaseWords));
+        }
+    }
+}

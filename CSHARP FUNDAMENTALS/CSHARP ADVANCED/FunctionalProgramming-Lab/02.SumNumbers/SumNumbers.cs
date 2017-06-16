3149660d048e8ad71677a2905237cbdeef0b19f8
Rostislav <radeff@radeff.eu>
2017-06-16 22:41:41 +0300

    Problem 02 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Lab/02.SumNumbers/SumNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    public class SumNumbers
+    {
+        public static void Main()
+        {
+            Func<string, int> NumberParser = n => int.Parse(n);
+            var numbers = Console.ReadLine()
+                .Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(NumberParser);
+
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}

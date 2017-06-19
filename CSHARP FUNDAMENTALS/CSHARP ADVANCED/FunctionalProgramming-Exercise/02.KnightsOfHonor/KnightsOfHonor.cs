d107b151838da8b0c7a3d64319b114f9c84b8ccc
Rostislav <radeff@radeff.eu>
2017-06-19 22:15:58 +0300

    Problem 02 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Exercise/02.KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02.KnightsOfHonor
+{
+    public class KnightsOfHonor
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine().Split(" \t".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            Action<string> addTitleAndPrint = s => Console.WriteLine(string.Concat("Sir ", s));
+            foreach (var name in names)
+            {
+                addTitleAndPrint(name);
+            }
+        }
+    }
+}

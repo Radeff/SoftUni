66ad971fcea0f4db417068036eaec65e8b7a3bcc
Rostislav <radeff@radeff.eu>
2017-06-19 22:11:28 +0300

    Problem 01 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Exercise/01.ActionPrint/ActionPrint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.ActionPrint
+{
+    public class ActionPrint
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine().Split();
+            Action<string> print = s => Console.WriteLine(s);
+            foreach (var name in names)
+            {
+                print(name);
+            }
+        }
+    }
+}

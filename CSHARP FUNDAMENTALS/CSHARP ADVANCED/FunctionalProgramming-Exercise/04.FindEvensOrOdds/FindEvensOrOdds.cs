c7b2665ea8e933e9bf838af0f65264b44f425e4e
Rostislav <radeff@radeff.eu>
2017-06-19 22:46:57 +0300

    Problem 04 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Exercise/04.FindEvensOrOdds/FindEvensOrOdds.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04.FindEvensOrOdds
+{
+    public class FindEvensOrOdds
+    {
+        public static void Main()
+        {
+            var range = Console.ReadLine().Split();
+            var start = int.Parse(range[0]);
+            var end = int.Parse(range[1]);
+            var oddEvenFilter = Console.ReadLine();
+
+            Predicate<int> odd = n => n % 2 != 0;
+            Predicate<int> even = n => n % 2 == 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                if (oddEvenFilter == "odd")
+                {
+                    if (odd(i))
+                    {
+                        Console.Write(string.Concat(i, " "));
+                    }
+                }
+                else
+                {
+                    if (even(i))
+                    {
+                        Console.Write(string.Concat(i, " "));
+                    }
+                }
+            }
+        }
+    }
+}

3afcf45131338f9a253b55fd158e27baf52db506
Rostislav <radeff@radeff.eu>
2017-06-19 22:32:59 +0300

    Problem 03 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Exercise/03.CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    public class CustomMinFunction
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], int> minFunc = ReturnMin;
+            Console.WriteLine(minFunc(numbers));
+        }
+
+        private static int ReturnMin(int[] arr)
+        {
+            var min = int.MaxValue;
+
+            foreach (var num in arr)
+            {
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+            return min;
+        }
+    }
+}

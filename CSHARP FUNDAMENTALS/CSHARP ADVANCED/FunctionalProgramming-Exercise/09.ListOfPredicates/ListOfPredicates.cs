5b34d014e50ce35a510353f6a584236cc2ca37b0
Rostislav <radeff@radeff.eu>
2017-06-20 19:45:08 +0300

    Problem 09 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Exercise/09.ListOfPredicates/ListOfPredicates.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    public class ListOfPredicates
+    {
+        public static void Main()
+        {
+            var end = int.Parse(Console.ReadLine());
+            var dividers = Console.ReadLine().Split().Select(int.Parse).Distinct().ToArray();
+            Func<int, int, bool> divisible = (n, d) => n % d == 0;
+            var result = new List<int>();
+            
+            for(int num = 1; num <= end; num++)
+            {
+                var check = true;
+
+                foreach (var divider in dividers)
+                {
+                    if (!divisible(num, divider))
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+
+                if (check)
+                {
+                    result.Add(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}

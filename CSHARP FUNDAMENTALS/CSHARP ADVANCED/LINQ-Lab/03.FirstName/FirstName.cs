de2fdf636b58b0f0722b2a57799c9598d4ebb354
Rostislav <radeff@radeff.eu>
2017-06-20 22:10:25 +0300

    Problem 03 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Lab/03.FirstName/FirstName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.FirstName
+{
+    public class FirstName
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine().Split().ToList();
+            var letters = Console.ReadLine()
+                .ToLower()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            var found = names.Where(n => letters.Contains(n.ToLower()[0].ToString()))
+                .OrderBy(n => n);
+
+            if (found.Count() > 0)
+            {
+                Console.WriteLine(found.First());
+            }
+            else
+            {
+                Console.WriteLine("No match");
+            }
+        }
+    }
+}

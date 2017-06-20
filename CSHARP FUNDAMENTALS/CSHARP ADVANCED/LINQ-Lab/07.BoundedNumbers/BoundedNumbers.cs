ef0dba19a5b096dd573863ed3ee03f7743a4403c
Rostislav <radeff@radeff.eu>
2017-06-20 22:43:50 +0300

    Problem 07 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Lab/07.BoundedNumbers/BoundedNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.BoundedNumbers
+{
+    public class BoundedNumbers
+    {
+        public static void Main()
+        {
+            var boundaries = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var lower = Math.Min(boundaries[0], boundaries[1]);
+            var upper = Math.Max(boundaries[0], boundaries[1]);
+            var numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+            var filtered = numbers.Where(x => x >= lower && x <= upper).ToList();
+            Console.WriteLine(string.Join(" ", filtered));
+        }
+    }
+}

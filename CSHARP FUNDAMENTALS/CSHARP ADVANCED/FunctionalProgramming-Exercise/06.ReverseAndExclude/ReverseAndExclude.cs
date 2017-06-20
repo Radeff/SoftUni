feefb659005e8433f289501a8855a03c903725c5
Rostislav <radeff@radeff.eu>
2017-06-20 18:02:39 +0300

    Problems 06 & 07 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Exercise/06.ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    public class ReverseAndExclude
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var divisor = int.Parse(Console.ReadLine());
+            Array.Reverse(numbers);
+            Predicate<int> divisible = n => n % divisor != 0;
+            
+            foreach (var num in numbers)
+            {
+                if (divisible(num))
+                {
+                    Console.Write(string.Concat(num, " "));
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Exercise/07.PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07.PredicateForNames
+{
+    public class PredicateForNames
+    {
+        public static void Main()
+        {
+            var length = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split();
+            Func<int, string, bool> nameLimit = (i, s) => s.Length <= i;
+            FilterAndPrintNames(length, names, nameLimit);
+        }
+
+        private static void FilterAndPrintNames(int length, string[] names, Func<int, string, bool> nameLimit)
+        {
+            foreach (var name in names)
+            {
+                if (nameLimit(length,name))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}

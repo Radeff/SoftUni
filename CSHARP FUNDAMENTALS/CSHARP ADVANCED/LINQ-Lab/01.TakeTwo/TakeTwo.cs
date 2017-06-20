9223d0a9e924563ba7d3451692ec6290da7406a1
Rostislav <radeff@radeff.eu>
2017-06-20 21:44:30 +0300

    Problem 01 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Lab/01.TakeTwo/TakeTwo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.TakeTwo
+{
+    public class TakeTwo
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            numbers.Select(x => x)
+                .Where(x => x >= 10 && x <= 20)
+                .Take(2)
+                .ToList()
+                .ForEach(x => Console.Write(string.Concat(x, " ")));
+            Console.WriteLine();
+        }
+    }
+}

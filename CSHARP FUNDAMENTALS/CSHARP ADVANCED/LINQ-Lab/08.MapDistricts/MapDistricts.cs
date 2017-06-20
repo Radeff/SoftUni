db48167aecee327b583f0f3ad0c6ff3c4bdfe49c
Rostislav <radeff@radeff.eu>
2017-06-20 23:06:23 +0300

    Problem 08 -Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Lab/08.MapDistricts/MapDistricts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.MapDistricts
+{
+    public class MapDistricts
+    {
+        public static void Main()
+        {
+            var data = Console.ReadLine().Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+            var cities = new Dictionary<string, List<long>>();
+
+            foreach (var pair in data)
+            {
+                var cityDistrict = pair.Split(':').ToArray();
+                var city = cityDistrict[0];
+                var population = long.Parse(cityDistrict[1]);
+
+                if (!cities.ContainsKey(city))
+                {
+                    var pop = new List<long>();
+                    cities.Add(city, pop);
+                }
+                cities[city].Add(population);
+            }
+
+            var limit = long.Parse(Console.ReadLine());
+            var filtered = cities.Where(kvp => kvp.Value.Sum() >= limit);
+
+            foreach (var kvp in filtered.OrderByDescending(kvp => kvp.Value.Sum()))
+            {
+                Console.WriteLine($"{kvp.Key}: {string.Join(" ", kvp.Value.OrderByDescending(x => x).Take(5))}");
+            }
+        }
+    }
+}

0fb4194dc5c9098948ef12a1f6a0815c93e4f184
Rostislav <radeff@radeff.eu>
2017-06-20 22:20:07 +0300

    Problem 05 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Lab/05.MinEvenNumber/MinEvenNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.MinEvenNumber
+{
+    public class MinEvenNumber
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+            var filteredEven = numbers.Where(x => x % 2 == 0).ToList();
+
+            if (filteredEven.Count > 0)
+            {
+                Console.WriteLine($"{filteredEven.Min():F2}");
+            }
+            else
+            {
+                Console.WriteLine("No match");
+            }
+        }
+    }
+}

f3952574f39b53577b2d3c224d58988fe8338de9
Rostislav <radeff@radeff.eu>
2017-06-20 18:53:16 +0300

    Problem 08 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/FunctionalProgramming-Exercise/08.CustomComparator/CustomComparator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.CustomComparator
+{
+    public class CustomComparator
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Array.Sort(numbers, (x, y) =>
+            {
+                if (x % 2 == 0 && y % 2 != 0)
+                {
+                    return -1;
+                }
+                if(x % 2 != 0 && y % 2 == 0)
+                {
+                    return 1;
+                }
+                if (x > y )
+                {
+                    return 1;
+                }
+                if (x < y)
+                {
+                    return -1;
+                }
+                return 0;
+            });
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}

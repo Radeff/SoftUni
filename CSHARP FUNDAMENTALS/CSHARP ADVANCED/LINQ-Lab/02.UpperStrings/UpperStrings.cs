b214bfbf0ef3fda142df186e982a01b636315b49
Rostislav <radeff@radeff.eu>
2017-06-20 21:48:21 +0300

    Problem 02 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Lab/02.UpperStrings/UpperStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.UpperStrings
+{
+    public class UpperStrings
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine().Split().ToList();
+            text.Select(w => w.ToUpper()).ToList().ForEach(w => Console.Write(string.Concat(w, " ")));
+            Console.WriteLine();
+        }
+    }
+}

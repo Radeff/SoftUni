5ae249a5b35ea4b00e0f49b24fcf611232d1fd57
Rostislav <radeff@radeff.eu>
2017-06-20 22:35:10 +0300

    Problem 06 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Lab/06.FindAndSumIntegers/FindAndSumIntegers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.FindAndSumIntegers
+{
+    public class FindAndSumIntegers
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split().ToList();
+            var n = 0L;
+            var ints = input.Where(s => long.TryParse(s, out n))
+                .Select(long.Parse)
+                .ToList();
+
+            if (ints.Count > 0)
+            {
+                Console.WriteLine(ints.Sum());
+            }
+            else
+            {
+                Console.WriteLine("No match");
+            }
+        }
+    }
+}

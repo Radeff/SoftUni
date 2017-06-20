e8cc9ea4b043d604741a5d25aab7bf52ae786e45
Rostislav <radeff@radeff.eu>
2017-06-20 22:13:54 +0300

    Problem 04 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Lab/04.AverageOfDoubles/AverageOfDoubles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AverageOfDoubles
+{
+    public class AverageOfDoubles
+    {
+        public static void Main()
+        {
+            var doubles = Console.ReadLine().Split()
+                .Select(double.Parse)
+                .ToList();
+            Console.WriteLine($"{doubles.Average():F2}");
+        }
+    }
+}

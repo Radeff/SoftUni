803f291b9a09a08e5ef92fa1978b3a56e5689175
Rostislav <radeff@radeff.eu>
2017-06-21 09:51:44 +0300

    Problem 02 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Exercise/02.StudentsByFirstAndLastName/StudentsByFirstAndLastName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.StudentsByFirstAndLastName
+{
+    public class StudentsByFirstAndLastName
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            while (input != "END")
+            {
+                var inputSplit = input
+                    .Split(" \t".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                if (inputSplit.Length == 2 && inputSplit[0].CompareTo(inputSplit[1]) < 0)
+                {
+                    Console.WriteLine(input);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}

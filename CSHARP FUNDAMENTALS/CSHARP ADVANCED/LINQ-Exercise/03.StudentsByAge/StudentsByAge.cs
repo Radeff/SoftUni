1b5a793421fcc87cd85dea61022096bf4bac7bd9
Rostislav <radeff@radeff.eu>
2017-06-21 09:57:30 +0300

    Problem 03 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Exercise/03.StudentsByAge/StudentsByAge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.StudentsByAge
+{
+    public class StudentsByAge
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split().ToArray();
+            var students = new Dictionary<string, int>();
+            while (input[0] != "END")
+            {
+                var name = string.Concat(input[0], " ", input[1]);
+                var age = int.Parse(input[2]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, age);
+                }
+
+                input = Console.ReadLine().Split().ToArray();
+            }
+
+            foreach (var student in students.Where(kvp => kvp.Value >= 18 && kvp.Value <= 24))
+            {
+                Console.WriteLine($"{student.Key} {student.Value}");
+            }
+        }
+    }
+}

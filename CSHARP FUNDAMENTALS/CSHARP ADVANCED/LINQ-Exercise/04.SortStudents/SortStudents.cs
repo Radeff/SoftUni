429cf64b705791b54a9550482b4fcb3b0fc0cba2
Rostislav <radeff@radeff.eu>
2017-06-21 10:06:26 +0300

    Problem 04 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Exercise/04.SortStudents/SortStudents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SortStudents
+{
+    public class SortStudents
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split().ToArray();
+            var students = new List<KeyValuePair<string,string>>();
+            while (input[0] != "END")
+            {
+                var firstName = input[0];
+                var lastName = input[1];
+                students.Add(new KeyValuePair<string, string>(firstName,lastName));
+
+                input = Console.ReadLine().Split().ToArray();
+            }
+
+            foreach (var student in students.OrderBy(s => s.Value).ThenByDescending(s => s.Key))
+            {
+                Console.WriteLine($"{student.Key} {student.Value}");
+            }
+        }
+    }
+}

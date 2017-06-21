1f1dd4ed9b7ea6d1ddd098ee979361437d1e18f7
Rostislav <radeff@radeff.eu>
2017-06-21 13:21:44 +0300

    Problem 06 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Exercise/06.FilterStudentsByPhone/FilterStudentsByPhone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.FilterStudentsByPhone
+{
+    public class FilterStudentsByPhone
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            while (input != "END")
+            {
+                var inputSplit = input.Split().ToArray();
+                if (inputSplit.Length == 3 && inputSplit[2].StartsWith("02") || inputSplit[2].StartsWith("+3592"))
+                {
+                    Console.WriteLine(string.Concat(inputSplit[0], " ", inputSplit[1]));
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}

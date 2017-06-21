12adc7c39f33387b3fa5cefa9cf974857c158de0
Rostislav <radeff@radeff.eu>
2017-06-21 10:10:29 +0300

    Problem 05 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Exercise/05.FilterStudentsByEmailDomain/FilterStudentsByEmailDomain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.FilterStudentsByEmailDomain
+{
+    public class FilterStudentsByEmailDomain
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            while (input != "END")
+            {
+                var inputSplit = input.Split().ToArray();
+                if (inputSplit.Length == 3 && inputSplit[2].EndsWith("@gmail.com"))
+                {
+                    Console.WriteLine(string.Concat(inputSplit[0], " ", inputSplit[1]));
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}

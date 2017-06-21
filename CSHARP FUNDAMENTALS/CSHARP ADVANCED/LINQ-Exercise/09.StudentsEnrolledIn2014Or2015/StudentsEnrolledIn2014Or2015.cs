d1f14b5da30fdce577d7464588b1efb7e0d991d1
Rostislav <radeff@radeff.eu>
2017-06-21 14:05:03 +0300

    Problem 09 - Solved

---
+++ CSHARP FUNDAMENTALS/CSHARP ADVANCED/LINQ-Exercise/09.StudentsEnrolledIn2014Or2015/StudentsEnrolledIn2014Or2015.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.StudentsEnrolledIn2014Or2015
+{
+    public class StudentsEnrolledIn2014Or2015
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split().ToArray();
+            var students = new Dictionary<string, List<string>>();
+            while (input[0] != "END")
+            {
+                var facNumber = input[0];
+                var grades = new List<string>()
+                {
+                    input[1],
+                    input[2],
+                    input[3],
+                    input[4],
+                };
+
+                if (!students.ContainsKey(facNumber))
+                {
+                    students.Add(facNumber, grades);
+                }
+
+                input = Console.ReadLine().Split().ToArray();
+            }
+
+            foreach (var student in students.Where(s => s.Key.EndsWith("14") || s.Key.EndsWith("15")))
+            {
+                Console.WriteLine(string.Join(" ", student.Value));
+            }
+        }
+    }
+}

4b20f720311930b9dc468cb503744952b069260a
Rostislav <radeff@radeff.eu>
2017-07-04 14:13:03 +0300

    Problem 09 - Solved

---
+++ CSHARP FUNDAMENTALS/OOP BASICS/DefiningClasses-Exercise/09.RectangleIntersection/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _09.RectangleIntersection
+{
+    public class Rectangle
+    {
+        private string id;
+        private double width;
+        private double height;
+        private double topLeftX;
+        private double topLeftY;
+
+        public string Id { get { return this.id; } }
+
+        public Rectangle(string id, double width, double height, double topLeftX, double topLeftY)
+        {
+            this.id = id;
+            this.width = Math.Abs(width);
+            this.height = Math.Abs(height);
+            this.topLeftX = topLeftX;
+            this.topLeftY = topLeftY;
+        }
+
+        public bool Intersection(Rectangle rectangle)
+        {
+            return rectangle.topLeftX + rectangle.width >= this.topLeftX &&
+                   rectangle.topLeftX <= this.topLeftX + this.width &&
+                   rectangle.topLeftY >= this.topLeftY - this.height &&
+                   rectangle.topLeftY - rectangle.height <= this.topLeftY;
+        }
+    }
+}
---
+++ CSHARP FUNDAMENTALS/OOP BASICS/DefiningClasses-Exercise/09.RectangleIntersection/RectangleIntersection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.RectangleIntersection
+{
+    public class RectangleIntersection
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            var n = int.Parse(input[0]);
+            var m = int.Parse(input[1]);
+            var rectangles = new List<Rectangle>();
+
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine().Split();
+                var rectangle = new Rectangle(
+                    input[0],
+                    double.Parse(input[1]),
+                    double.Parse(input[2]),
+                    double.Parse(input[3]),
+                    double.Parse(input[4])
+                    );
+
+                rectangles.Add(rectangle);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                var pairToCheck = Console.ReadLine().Split();
+                var firstRect = rectangles.First(r => r.Id == pairToCheck[0]);
+                var secondRect = rectangles.First(r => r.Id == pairToCheck[1]);
+                
+                if (firstRect.Intersection(secondRect))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+    }
+}

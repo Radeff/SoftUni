eb595d84b57ac1449e74f101669a81b71a8d5812
Rostislav <radeff@radeff.eu>
2017-07-05 15:11:54 +0300

    Problem 04 - Solved

---
+++ CSHARP FUNDAMENTALS/OOP BASICS/Encapsulation-Lab/04.FirstАndReserveTeam/FirstAndReserveTeam.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04.FirstAndReserveTeam
+{
+    public class FirstAndReserveTeam
+    {
+        public static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var team = new Team("PFC Dunav Ruse");
+            for (int i = 0; i < lines; i++)
+            {
+                try
+                {
+                    var cmdArgs = Console.ReadLine().Split();
+                    var person = new Person(cmdArgs[0],
+                        cmdArgs[1],
+                        int.Parse(cmdArgs[2]),
+                        double.Parse(cmdArgs[3]));
+
+                    team.AddPlayer(person);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine($"First team have {team.FirstTeam.Count} players");
+            Console.WriteLine($"Reserve team have {team.ReserveTeam.Count} players");
+        }
+    }
+}
---
+++ CSHARP FUNDAMENTALS/OOP BASICS/Encapsulation-Lab/04.FirstАndReserveTeam/Team.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace _04.FirstAndReserveTeam
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get { return this.firstTeam; }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam; }
+        }
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age < 40)
+            {
+                firstTeam.Add(player);
+            }
+            else
+            {
+                reserveTeam.Add(player);
+            }
+        }
+    }
+}

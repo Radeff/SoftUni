eeb0ff27637502255a1d660b0472549d3bc1827d
Rostislav <radeff@radeff.eu>
2017-07-05 14:19:21 +0300

    Problem 03 - Solved

---
+++ CSHARP FUNDAMENTALS/OOP BASICS/Encapsulation-Lab/03.ValidationData/Person.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace _03.ValidationData
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private double salary;
+
+        public Person(string firstName, string lastName, int age, double salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot be less than 3 symbols");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot be less than 3 symbols");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or negative integer");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public double Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            set
+            {
+                if (value < 460.0)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva");
+                }
+
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(double bonus)
+        {
+            if (this.age > 30)
+            {
+                this.salary += this.salary * bonus / 100.0;
+            }
+            else
+            {
+                this.salary += this.salary * (bonus / 100.0) / 2.0;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} get {this.Salary:F2} leva";
+        }
+    }
+}

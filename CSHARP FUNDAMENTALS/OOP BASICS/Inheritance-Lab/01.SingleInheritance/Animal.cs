e332434897dc6b3c880168399fb8adb99cd809ab
Rostislav <radeff@radeff.eu>
2017-07-08 01:16:32 +0300

    Problem 01 - Solved

---
+++ CSHARP FUNDAMENTALS/OOP BASICS/Inheritance-Lab/01.SingleInheritance/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.SingleInheritance
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
---
+++ CSHARP FUNDAMENTALS/OOP BASICS/Inheritance-Lab/01.SingleInheritance/SingleInheritance.cs
@@ -0,0 +1,12 @@
+﻿namespace _01.SingleInheritance
+{
+    public class SingleInheritance
+    {
+        public static void Main()
+        {
+            var dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+}

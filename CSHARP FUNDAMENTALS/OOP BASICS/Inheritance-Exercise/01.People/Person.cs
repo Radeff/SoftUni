1b7be72e211d34c382db11a3a2b1a35b3fc966e0
Rostislav <radeff@radeff.eu>
2017-07-08 04:07:50 +0300

    Problem 01 - Solved

---
+++ CSHARP FUNDAMENTALS/OOP BASICS/Inheritance-Exercise/01.People/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _01.People
+{
+    public class Person
+    {
+        private const int MinimumNameLength = 3;
+
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (value.Length < MinimumNameLength)
+                {
+                    throw new ArgumentException($"Name's length should not be less than {MinimumNameLength} symbols!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public virtual int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age must be positive!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
